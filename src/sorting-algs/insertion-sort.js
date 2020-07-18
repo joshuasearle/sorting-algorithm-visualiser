47147e4c06808e27f91870495322563b5ede38a8
Josh Searle <joshryansearle@gmail.com>
2020-07-18 21:18:06 +1000

    changed insertion sort so that is also cancels properly

--- src/sorting-algs/insertion-sort.js
+++ src/sorting-algs/insertion-sort.js
@@ -1,52 +1,58 @@
 import * as actionCreators from '../store/actionCreators';
 
-const insertionSort = (arr) => {
+const insertionSort = (arr, visId) => {
   const animations = [];
   const highlights = arr.map((item, idx) => ({
     idx: idx,
     color: 'black',
   }));
-  animations.push(actionCreators.highlightElements(highlights));
+  animations.push(actionCreators.highlightElements(highlights, visId));
   const result = [...arr];
   for (let i = 0; i < arr.length; i++) {
     animations.push(
-      actionCreators.highlightElements([{ idx: i, color: 'green' }])
+      actionCreators.highlightElements([{ idx: i, color: 'green' }], visId)
     );
     let j = i - 1;
     animations.push(
-      actionCreators.highlightElements([{ idx: j, color: 'green' }])
+      actionCreators.highlightElements([{ idx: j, color: 'green' }], visId)
     );
     while (j >= 0 && result[j + 1].value < result[j].value) {
       let tmp = result[j];
       result[j] = result[j + 1];
       result[j + 1] = tmp;
-      animations.push(actionCreators.swapElements(j, j + 1));
+      animations.push(actionCreators.swapElements(j, j + 1, visId));
       animations.push(
-        actionCreators.highlightElements([
-          { idx: j + 1, color: 'black' },
-          { idx: j - 1, color: 'green' },
-        ])
+        actionCreators.highlightElements(
+          [
+            { idx: j + 1, color: 'black' },
+            { idx: j - 1, color: 'green' },
+          ],
+          visId
+        )
       );
 
       j -= 1;
     }
     animations.push(
-      actionCreators.highlightElements([
-        { idx: j, color: 'black' },
-        { idx: j + 1, color: 'black' },
-      ])
+      actionCreators.highlightElements(
+        [
+          { idx: j, color: 'black' },
+          { idx: j + 1, color: 'black' },
+        ],
+        visId
+      )
     );
   }
 
   for (let i = 0; i < arr.length; i++) {
     animations.push(
-      actionCreators.highlightElements([{ idx: i, color: 'green' }])
+      actionCreators.highlightElements([{ idx: i, color: 'green' }], visId)
     );
   }
 
   for (let i = arr.length; i >= 0; i--) {
     animations.push(
-      actionCreators.highlightElements([{ idx: i, color: 'black' }])
+      actionCreators.highlightElements([{ idx: i, color: 'black' }], visId)
     );
   }
 

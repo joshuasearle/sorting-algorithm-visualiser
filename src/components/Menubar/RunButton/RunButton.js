a0e33e667b4947b97aa8906fe24f18a4971c682f
Josh Searle <joshryansearle@gmail.com>
2020-07-20 12:23:56 +1000

    removed redundent code from RunButton

--- src/components/Menubar/RunButton/RunButton.js
+++ src/components/Menubar/RunButton/RunButton.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Button from '../../inputs/Button/Button';
 import * as actionCreators from '../../../store/actionCreators';
 
 const RunButton = () => {
   const dispatch = useDispatch();
-  const [currentList, algorithm] = useSelector((state) => [
-    state.currentList,
-    state.algorithm,
-  ]);
 
   const buttonPressHandler = () => {
     dispatch(actionCreators.visualise());

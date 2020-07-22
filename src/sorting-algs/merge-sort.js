c7c690431b0fdbd325c6aa20a31123d96a624eaa
Josh Searle <joshryansearle@gmail.com>
2020-07-22 22:18:35 +1000

    added inplace merge sort, as animations are impossible without it

--- src/sorting-algs/merge-sort.js
+++ src/sorting-algs/merge-sort.js
@@ -1,68 +1,44 @@
-const mergeSort = (arr, visId) => {
-  if (arr.length > 1) {
-    const mid = arr.length / 2;
-    let left = arr.slice(0, mid);
-    let right = arr.slice(mid);
+const merge = (arr, lIdx, mid, rIdx, visId, animations) => {
+  // start of left sub arr
+  let lStart = lIdx;
 
-    left = mergeSort(left);
-    right = mergeSort(right);
+  // start of right sub arr
+  let rStart = mid + 1;
 
-    let i = 0;
-    let j = 0;
-    let k = 0;
+  // as both halves are sorted, if this holds, no merge is needed
+  if (arr[mid] <= arr[rStart]) {
+    return;
+  }
 
-    const result = Array(arr.length).fill(null);
-
-    while (i < left.length && j < right.length) {
-      if (left[i].value < right[j].value) {
-        result[k] = left[i];
-        i++;
-      } else {
-        result[k] = right[j];
-        j++;
+  while (lStart <= mid && rStart <= rIdx) {
+    if (arr[lStart] <= arr[rStart]) {
+      lStart++;
+    } else {
+      let idx = rStart;
+      while (idx >= lStart + 1) {
+        [arr[idx], arr[idx - 1]] = [arr[idx - 1], arr[idx]];
+        idx--;
       }
-      k++;
+      lStart++;
+      mid++;
+      rStart++;
     }
+  }
+};
 
-    while (i < left.length) {
-      result[k] = left[i];
-      i++;
-      k++;
-    }
-
-    while (j < right.length) {
-      result[k] = right[j];
-      j++;
-      k++;
-    }
-    return result;
+const mergeSortAux = (arr, lIdx, rIdx, visId, animations) => {
+  if (lIdx < rIdx) {
+    const mid = Math.floor(lIdx + (rIdx - lIdx) / 2);
+    mergeSortAux(arr, lIdx, mid, visId, animations);
+    mergeSortAux(arr, mid + 1, rIdx, visId, animations);
+    merge(arr, lIdx, mid, rIdx, visId, animations);
   }
   return arr;
 };
 
-// const test = [4, 5, 6, 7, 3, 3, 4, 56, 67, 4, 2];
-
-// for (let i = 0; i < 1000; i++) {
-//   let test = [...Array.from(Array(1000), () => Math.random())];
-//   let jsSorted = [...test].sort((a, b) => a - b);
-//   let mergeSorted = mergeSort([...test]);
-//   console.log(arraysEqual(jsSorted, mergeSorted));
-// }
-
-// function arraysEqual(a, b) {
-//   if (a === b) return true;
-//   if (a == null || b == null) return false;
-//   if (a.length !== b.length) return false;
-
-//   // If you don't care about the order of the elements inside
-//   // the array, you should sort both arrays here.
-//   // Please note that calling sort on an array will modify that array.
-//   // you might want to clone your array first.
-
-//   for (var i = 0; i < a.length; ++i) {
-//     if (a[i] !== b[i]) return false;
-//   }
-//   return true;
-// }
-
-// console.log(mergeSort(test));
+const mergeSort = (arr, visId) => {
+  const animations = [];
+  const result = [...arr];
+  mergeSortAux(result, 0, arr.length - 1, visId, animations);
+  return result;
+};

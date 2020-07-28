b74ba5ce215b569ee3cadfc509033bfa4d493f05
Josh Searle <joshryansearle@gmail.com>
2020-07-28 12:30:27 +1000

    change color theme to purple

--- src/components/Columns/Column/Column.js
+++ src/components/Columns/Column/Column.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './Column.module.css';
 
 const Column = ({ value, width, left, color }) => {
+  color = color === 'green' ? '#9900a1' : 'black';
   const styles = {
     backgroundColor: color,
     width: `${width}px`,

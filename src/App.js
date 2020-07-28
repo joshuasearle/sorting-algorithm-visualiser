895014886431686a4229b4448aac6b2eb92793d7
Josh Searle <joshryansearle@gmail.com>
2020-07-28 12:52:48 +1000

    add footer to appjs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Menubar from './components/Menubar/Menubar';
 import Columns from './components/Columns/Columns';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Menubar />
       <Columns />
+      <Footer />
     </>
   );
 };
